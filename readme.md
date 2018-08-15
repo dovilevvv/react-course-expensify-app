b9745194cba546812242395610d5cfb947f95f82
dovilevvv <vil.dovile@gmail.com>
2018-08-15 14:24:36 +0300

    Setup prod build and server

---
+++ readme.md
@@ -0,0 +1,5 @@
+git init - Create a new git repo
+git status - view the changes to your project code
+git add - Add files to staging are
+git commit - Creates a new commit with files from staging area
+git log - View recent commits
---
+++ server/server.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+// create express application
+const app = express();
+const publicPath = path.join(__dirname, '../', 'public');
+
+// for haruki
+const port = process.enc.PORT || 3000;
+
+// use public directory to serve static assets
+app.use(express.static(publicPath));
+
+// if request is in public folder, get back index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+// start on port 3000
+app.listen(port, () => {
+    console.log('Server is up!');
+});
+
+//command: node server/server.js
