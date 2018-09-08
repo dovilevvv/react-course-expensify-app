6cd38b34a07eb7fcc4de8ccc45daf34a40ccc68f
dovilevvv <vil.dovile@gmail.com>
2018-09-08 19:05:19 +0300

    fixing setup test database env

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
-process.env.NODE_ENV=proccess.env.NODE_ENV || 'development';
+process.env.NODE_ENV=process.env.NODE_ENV || 'development';
 
 if(process.env.NODE_ENV === 'test'){
     require('dotenv').config({path: '.env.test'});
